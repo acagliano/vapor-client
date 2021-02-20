15e4769ab08de01dbf4ecedf826e65edd3d9723a
Anthony Cagliano <acagliano97@gmail.com>
2021-02-19 22:52:13 -0500

    Add some fixes to the reload function to try to fix. Now we don't crash on reentry, but we do crash on quit from reloaded

--- src/asm/launch_self.asm
+++ src/asm/launch_self.asm
@@ -1,5 +1,3 @@
-
-
 extern _ti_Open
 extern _ti_Close
 extern _ti_OpenVar
@@ -7,6 +5,7 @@ extern _ti_Write
 extern _ti_GetDataPtr
 extern _ti_GetSize
 extern _gfx_End
+extern __exitsp
 
 public _update_program
 
@@ -18,63 +17,69 @@ _EnoughMem     := $02051C
 
 ;void update_program(void);
 _update_program:
-	ld a,($E30018)
-	cp a,$2D ;OS 16bpp
-	call nz,_gfx_End
+    ld sp,(__exitsp)
+    ld a,($E30018)
+    cp a,$2D ;OS 16bpp
+    call nz,_gfx_End
     ld hl, $06
     push hl
-	ld hl,mode_r
-	push hl
-	ld hl,program_name
-	push hl
-	call _ti_OpenVar
-	pop bc
-	pop bc
+    ld hl,mode_r
+    push hl
+    ld hl,program_name
+    push hl
+    call _ti_OpenVar
     pop bc
-	or a,a
-	ret z
-	ld c,a
-	push bc
-	call _ti_GetDataPtr
-	ld (dataptr),hl
-	call _ti_GetSize
-	ld (datasize),hl
-	call _ti_Close
-	pop bc
-	ld hl,jump_data
-	ld de,jump_data_loc
-	ld bc,jump_data_len
-	push de
-	ldir
-	ret
-	
+    pop bc
+    pop bc
+    or a,a
+    ret z
+    ld c,a
+    push bc
+    call _ti_GetDataPtr
+    inc hl
+    inc hl
+    ld (dataptr),hl
+    call _ti_GetSize
+    dec hl
+    dec hl
+    ld (datasize),hl
+    call _ti_Close
+    pop bc
+    ld hl,jump_data
+    ld de,jump_data_loc
+    ld bc,jump_data_len
+    push de
+    ldir
+    ret
+    
 jump_data_loc:=$E30800
 jump_data:
-	or a,a
-	sbc hl,hl
-	ld de,(_asm_prgm_size)
-	ld (_asm_prgm_size),hl
-	ld hl,_userMem
-	call _DelMem
-	call _EnoughMem
-	ret c
-	ld hl,0
+    or a,a
+    sbc hl,hl
+    ld de,(_asm_prgm_size)
+    ld (_asm_prgm_size),hl
+    ld hl,_userMem
+    call _DelMem
+    ld hl,0
 datasize:=$-3
-	ld (_asm_prgm_size),hl
-	push hl
-	ld de,_userMem
-	push de
-	call _InsertMem
-	pop de
-	pop bc
-	push de
-	ld hl,0
+    call _EnoughMem ;returns HL in DE
+    ret c
+    ex hl,de
+    ld (_asm_prgm_size),hl
+    push hl
+    ld de,_userMem
+    push de
+    call _InsertMem
+    pop de
+    pop bc
+    push de
+    ld hl,0
 dataptr:=$-3
-	ldir
-	ret
+    ldir
+    ret
 jump_data_len:=$-jump_data
 
 mode_r:
-	db "r",0
+    db "r",0
 program_name:
-	db "VAPOR",0
+    db "VAPOR",0
